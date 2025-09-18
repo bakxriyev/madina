a7e8feee6e7aaea6c96faa1985590635f3a8ebce
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-09-19 00:06:44 +0500

    dssds

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import LandingPage2 from "@/components/landing-page2"
 import LandingPage from "../components/landing-page"
 
 export default function Page() {
-  return <LandingPage2 />
+  return <LandingPage />
 }
