5a2439e54787d13d9531da6c5fb233271a469305
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-09-24 11:31:45 +0500

    dfdf

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import LandingPage2 from "@/components/landing-page2"
 import LandingPage from "../components/landing-page"
+import LandingPage3 from "@/components/landing-page3"
 
 export default function Page() {
-  return <LandingPage2/>
+  return <LandingPage3/>
 }
