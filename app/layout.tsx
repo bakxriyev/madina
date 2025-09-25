cbe708e2c4e4709fb42d5598ed5a16cf8cfd8915
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-09-25 20:17:53 +0500

    sdsd

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,8 @@ export default function RootLayout({
           {`
             !function(f,b,e,v,n,t,s){
               if(f.fbq)return;
-              n=f.fbq=function(){n.callMethod ? n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
+              n=f.fbq=function(){n.callMethod ? 
+              n.callMethod.apply(n,arguments) : n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;
               n.push=n;
               n.loaded=!0;
@@ -58,6 +59,43 @@ export default function RootLayout({
           />
         </noscript>
         {/* End Meta Pixel Code */}
+
+        {/* Yandex.Metrika counter */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),
+              a=e.getElementsByTagName(t)[0],
+              k.async=1,
+              k.src=r,
+              a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104283397', 'ym');
+
+            ym(104283397, 'init', {
+              ssr:true,
+              webvisor:true,
+              clickmap:true,
+              ecommerce:"dataLayer",
+              accurateTrackBounce:true,
+              trackLinks:true
+            });
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/104283397"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* End Yandex.Metrika counter */}
 
         <Analytics />
       </body>
